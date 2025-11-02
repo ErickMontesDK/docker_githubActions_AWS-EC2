86e4359307a3eafac1dd8b07eac972406cf35590
ErickDKnight <erickmontesdk@gmail.com>
2025-11-01 23:18:53 -0700

    editing text

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hello World.
+            Hello World!
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            This is a sample project by Erick Montes Bedolla
+            This is a sample project by Erick Montes Bedolla using Next.js.
          
           </p>
         </div>
